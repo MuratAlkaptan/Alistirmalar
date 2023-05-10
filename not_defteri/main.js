4d4c5ea3c4fa61149c01fd79e41aadd359053116
MuratAlkaptan <131607763+MuratAlkaptan@users.noreply.github.com>
2023-05-11 01:15:33 +0300

    Update main.js

--- not_defteri/main.js
+++ not_defteri/main.js
@@ -1,12 +1,34 @@
-console.log("Hello!")
+//gets the uploaded document and reads it to the textarea 
+var file = document.getElementById('myFile').addEventListener('change', function(){
+  var fr = new FileReader();
+  fr.onload=function(){
+    document.getElementById('notDefteri').textContent=fr.result;
 
-var file = document.getElementById("myFile").files[0];
+  }
+  fr.readAsText(this.files[0]);
+})
 
-var reader = new FileReader();
-reader.onload = function (e){
+//when the download button is clicked reads the textarea and turns it into a blob that is downloadable as a .txt file
+document.getElementById('download').addEventListener('click', function(){
+  const value = notDefteri.value;
+  var blob = new Blob([value], {type:'text/plain'});
+  var url = URL.createObjectURL(blob);
+  
+  window.open(url, '_blank')
+    saveAs(url, "text.txt")
+});
 
-    var textArea = document.getElementById("myFile");
-    textArea.value = e.target.result;
+const saveAs = (url, filename) => {
+  const link = document.createElement('a');
+
+  if(link.download===undefined) throw new Error('HTML5 download is not supported');
+
+  link.setAttribute('href', url);
+  link.setAttribute('download', filename);
+  link.style.visibility='hidden';
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 
 };
-reader.readAsText(file);
