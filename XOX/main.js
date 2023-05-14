c754aa0336e0c192afa2826a4fc153b52ec6cbb7
MuratAlkaptan <131607763+MuratAlkaptan@users.noreply.github.com>
2023-05-15 00:20:39 +0300

    Add files via upload

--- XOX/main.js
+++ XOX/main.js
@@ -1,43 +1,72 @@
-const Columns = 'I I I I' // Grid elements
-const Rows = '_____'
-let Empty= '.'
+//kazanma koşullarının sıralaması
+const conditions = [
 
-//Creating the grid
-document.querySelector('#header1').innerHTML = Columns;
+    [0,1,2],
+    [0,4,8],
+    [0,3,6],
+    [3,4,5],
+    [6,7,8],
+    [6,4,2],
+    [1,4,7],
+    [2,5,8]
 
-document.querySelector('#header2').innerHTML = Columns;
+];
 
-document.querySelector('#header3').innerHTML = Columns;
+//Hücrelerin içindeki datayı taşıyan değişken
+const cells = document.querySelectorAll("td");
 
-document.querySelector('#header4').innerHTML = Rows;
-document.querySelector('#header5').innerHTML = Rows;
-document.querySelector('#header6').innerHTML = Rows;
-document.querySelector('#header7').innerHTML = Rows;
-document.querySelector('#header8').innerHTML = Empty;
-document.querySelector('#header9').innerHTML = Empty;
-document.querySelector('#header10').innerHTML = Empty;
-document.querySelector('#header11').innerHTML = Empty;
-document.querySelector('#header12').innerHTML = Empty;
-document.querySelector('#header13').innerHTML = Empty;
-document.querySelector('#header14').innerHTML = Empty;
-document.querySelector('#header15').innerHTML = Empty;
-document.querySelector('#header16').innerHTML = Empty;
+//Sırası olan oyuncunun işareti
+let player = 'X';
 
-let player=0;
+//Bu fonksiyon oyunun bitip bitmediğini kontrol eder
+function winCondition(player, conditions){
 
-if(player=0){document.getElementByClass('Empty').addEventListener(click, function(){
+    for(let i=0; i<=conditions; i++){
+
+        //koşulları döngüyle kontrol et
+        const [c1, c2, c3] = conditions[i];
+
+        //herhangi koşulu sağlayan 3 kutucuk aynı işaretli ise oyunu bitir
+        if(cells[c1] === player && 
+            cells[c2] === player &&
+            cells[c3] === player){
+
+                //hangi oyuncunun kazandığını belirt
+                if(player === 'X'){
+                    document.getElementById("prompt").innerHTML = 'Player X has won!'
+                }
+                else{
+                    document.getElementById("prompt").innerHTML = 'Player O has won!'  
+                }
+
+        }
+
+    }
+
+
+}
+
+//Tıklandığı zaman hücrenin içine gerekli işareti yazdıracak fonksiyon
+function onCellClick(player){
+
+    //td'ye data olarak player işaretini yazdır
+    document.getElementById("0").append(player);//player değişkenini yazdıramıyorum ama string yazdırıyor?
+}
+
+//Oyuncu üzerine tıkladığı zaman gerekli fonksiyonları çalıştır
+document.getElementById("0").addEventListener('click', function(player){//0 kullanmak yerine bütün hücreleri kontrol edecek bir yol?
+   
     
-    //print an x where a dot would be
-    Empty='x';
-    player++;
-
-})}
-
-else {document.getElementByClass('Empty').addEventListener(click, function(){
+    onCellClick(player);
+    winCondition(player, conditions);
     
-    //print an o where a dot would be
-    Empty='o';
-    player--;
+    
+    //Oyuncu tıkladıktan sonra sırayı değiştir
+    if(player === 'X'){
+        player = 'O';
+    }
+    else{
+        player = 'X';
+    }
 
-})}
-
+});
