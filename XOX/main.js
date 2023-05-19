45008c81bd94f92c02ae8d610e0337b0485a8fa7
MuratAlkaptan <131607763+MuratAlkaptan@users.noreply.github.com>
2023-05-19 13:21:29 +0300

    Add files via upload

--- XOX/main.js
+++ XOX/main.js
@@ -13,24 +13,24 @@ const conditions = [
 ];
 
 //Hücrelerin içindeki datayı taşıyan değişken
-const cells = document.getElementsByClassName("td");
+const cells = document.querySelectorAll("td");
 
 
 //Sırası olan oyuncunun işareti
 let player = "X";
 
 //Bu fonksiyon oyunun bitip bitmediğini kontrol eder
-function winCondition(conditions){
+function winCondition(){
 
-    for(let i=0; i<=conditions; i++){
+    for(let i=0; i<7; i++){
 
         //koşulları döngüyle kontrol et
         const [c1, c2, c3] = conditions[i];
 
         //herhangi koşulu sağlayan 3 kutucuk aynı işaretli ise oyunu bitir
-        if(cells[c1] === player && 
-            cells[c2] === player &&
-            cells[c3] === player){
+        if(cells[c1] == player && 
+            cells[c2] == player &&
+            cells[c3] == player){
 
                 //hangi oyuncunun kazandığını belirt
                 if(player == "X"){
@@ -50,15 +50,16 @@ function winCondition(conditions){
 //Tıklandığı zaman hücrenin içine gerekli işareti yazdıracak fonksiyon
 function onCellClick(x){
     
+    document.getElementById(x.toString()).append(player);
+
         if(player == "X"){
 
-            document.getElementById(x.toString()).append("X");
             //Oyuncu tıkladıktan sonra sırayı değiştir
             player = "O";
-            
         }
+        
         else{
-            document.getElementById(x.toString()).append("O");
+
             //Oyuncu tıkladıktan sonra sırayı değiştir
             player = "X";
             
@@ -75,6 +76,6 @@ for(let cellId of cells){
    
     let whichCell = event.target.id
     onCellClick(whichCell);
-    winCondition(conditions);
+    winCondition();
     
 })};
