6a3167813d3caa9841782f3fefa0d9f0565b46d6
MuratAlkaptan <131607763+MuratAlkaptan@users.noreply.github.com>
2023-07-03 12:17:22 +0300

    Website Form
    Sign-in and Sign-up forms

--- WebSite/website.js
+++ WebSite/website.js
@@ -1,23 +1,56 @@
 const slideDiv = document.getElementById("imageDiv");
-let slideBtn = document.getElementById("slider");
+const container = document.getElementById("boxContainer");
+const slideBtn = document.getElementById("slider");
+
+const starter = () => {
+    container.classList.toggle("divStart")
+    slideDiv.classList.toggle("imageStart")
+}
+
+const opening = () => {
+    container.classList.remove("divStart")
+    slideDiv.classList.remove("imageStart")
+
+    container.classList.toggle("divOpen")
+    slideDiv.classList.toggle("imageOpen")
+}
+
+const upSwitch = () => {
+    container.classList.remove("divOpen")
+    slideDiv.classList.remove("imageOpen")
+
+    container.classList.toggle("divClose")
+    slideDiv.classList.toggle("imageClose")
+}
+const leanRight = () => {
+    slideDiv.classList.toggle("marginRight")
+}
+
+starter()
+
+setTimeout(opening, 200)
+
+document.getElementById("passwordText").addEventListener("click", () => {
+    window.open('_blank');
+})
 
 function imgSlider(){
     {
+
+        upSwitch()
         if(slideBtn.textContent == "SIGN UP"){
-            if(slideDiv.className=="rightSlide"){
-                slideDiv.classList.toggle("rightSlide")
-            }
-            slideDiv.classList.toggle("leftSlide")
-            slideBtn.textContent = "SIGN IN"
+            setTimeout(opening, 1000)
             
+            setTimeout(leanRight, 1000)
+            slideBtn.textContent = "SIGN IN"
         }
         else if(slideBtn.textContent == "SIGN IN"){
-            if(slideDiv.className=="leftSlide"){
-                slideDiv.classList.toggle("leftSlide")
-            }
-            slideDiv.classList.toggle("rightSlide")
+            setTimeout(opening, 1000)
+
+            setTimeout(leanRight, 1000)
             slideBtn.textContent = "SIGN UP"
         }
+        setTimeout(upSwitch, 2000)
     }
 }
 slideBtn.addEventListener('click', imgSlider);
